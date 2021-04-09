c5d34a705b6c392d0fcd2b4d1ae33104d976ddd3
Atharv <rajeshbehera489@gmail.com>
2021-04-09 20:59:15 +0530

    Second change

--- newsletter.js
+++ newsletter.js
@@ -1,17 +1,22 @@
+// requiring modules here
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const https = require("https");
 
+// app creation
 const app = express();
 
+// app use
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// app get home
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/signup.html");
 });
 
+// app post home
 app.post("/", function (req, res) {
   const fName = req.body.fname;
   const lName = req.body.lname;
@@ -45,16 +50,18 @@ app.post("/", function (req, res) {
     } else {
       res.sendFile(__dirname + "/failure.html");
     }
-
-    response.on("data", function (data) {
-      console.log(JSON.parse(data));
-    });
   });
 
   request.write(jsonData);
   request.end();
 });
 
+// app post failure
+app.post("/failure", function (req, res) {
+  res.redirect("/");
+});
+
+// app listen
 app.listen(process.env.PORT || 4990, function () {
   console.log("The server is running on port 4990");
 });
